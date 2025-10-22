6a7d0a86e0562bbe3ecae3daec67a1cf08f24a9d
Winphillips <98627220+Winphillips@users.noreply.github.com>
2025-10-22 12:59:13 -0500

    Deploy site

--- lariat-static-site/vite.config.ts
+++ lariat-static-site/vite.config.ts
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lariat-website/',
+  base: '/',
   plugins: [
     vue(),
     // vueDevTools(),
